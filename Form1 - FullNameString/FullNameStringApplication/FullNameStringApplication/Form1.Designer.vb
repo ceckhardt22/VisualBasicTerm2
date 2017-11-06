fd5b77dc1f77297b5b0209458675c6da5e38076d
Chane Eckhardt <33033313+ceckhardt22@users.noreply.github.com>
2017-11-06 11:25:13 -0600

    Add files via upload

---
+++ Form1 - FullNameString/FullNameStringApplication/FullNameStringApplication/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnShowName = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExitApp = New System.Windows.Forms.Button()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.lblFullName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblFullNameString = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnShowName
+        '
+        Me.btnShowName.Location = New System.Drawing.Point(12, 227)
+        Me.btnShowName.Name = "btnShowName"
+        Me.btnShowName.Size = New System.Drawing.Size(85, 35)
+        Me.btnShowName.TabIndex = 0
+        Me.btnShowName.Text = "Show Name"
+        Me.btnShowName.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(155, 227)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(85, 35)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExitApp
+        '
+        Me.btnExitApp.Location = New System.Drawing.Point(296, 227)
+        Me.btnExitApp.Name = "btnExitApp"
+        Me.btnExitApp.Size = New System.Drawing.Size(83, 35)
+        Me.btnExitApp.TabIndex = 2
+        Me.btnExitApp.Text = "Exit"
+        Me.btnExitApp.UseVisualStyleBackColor = True
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFirstName.Location = New System.Drawing.Point(51, 33)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(141, 16)
+        Me.lblFirstName.TabIndex = 3
+        Me.lblFirstName.Text = "Enter you first name:"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.txtFirstName.Location = New System.Drawing.Point(198, 33)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(149, 20)
+        Me.txtFirstName.TabIndex = 7
+        '
+        'txtLastName
+        '
+        Me.txtLastName.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.txtLastName.Location = New System.Drawing.Point(198, 104)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(149, 20)
+        Me.txtLastName.TabIndex = 8
+        '
+        'lblFullName
+        '
+        Me.lblFullName.AutoSize = True
+        Me.lblFullName.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFullName.Location = New System.Drawing.Point(51, 171)
+        Me.lblFullName.Name = "lblFullName"
+        Me.lblFullName.Size = New System.Drawing.Size(102, 16)
+        Me.lblFullName.TabIndex = 10
+        Me.lblFullName.Text = "Your full name:"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLastName.Location = New System.Drawing.Point(51, 104)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(140, 16)
+        Me.lblLastName.TabIndex = 11
+        Me.lblLastName.Text = "Enter you last name:"
+        '
+        'lblFullNameString
+        '
+        Me.lblFullNameString.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.lblFullNameString.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFullNameString.Font = New System.Drawing.Font("MS Reference Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFullNameString.Location = New System.Drawing.Point(159, 171)
+        Me.lblFullNameString.Name = "lblFullNameString"
+        Me.lblFullNameString.Size = New System.Drawing.Size(220, 23)
+        Me.lblFullNameString.TabIndex = 12
+        Me.lblFullNameString.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(391, 293)
+        Me.Controls.Add(Me.lblFullNameString)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblFullName)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.btnExitApp)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnShowName)
+        Me.Name = "Form1"
+        Me.Text = "Your Full Name"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnShowName As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExitApp As System.Windows.Forms.Button
+    Friend WithEvents lblFirstName As System.Windows.Forms.Label
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents lblFullName As System.Windows.Forms.Label
+    Friend WithEvents lblLastName As System.Windows.Forms.Label
+    Friend WithEvents lblFullNameString As System.Windows.Forms.Label
+
+End Class
---
+++ Form1 - FullNameString/FullNameStringApplication/FullNameStringApplication/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnShowName.Click
+        ' This declare's a string variable to hold the full name...
+        Dim strFullName As String
+
+        ' ...this combine;s the first and last names and assign the
+        ' result to strFullName...
+        strFullName = txtFirstName.Text & " " & txtLastName.Text
+
+        ' ...and this display's the full name in the lblFullName label.
+        lblFullNameString.Text = strFullName
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' This clears TextBox controls and the Label...
+        txtFirstName.Clear()
+        txtLastName.Clear()
+        lblFullNameString.Text = String.Empty
+
+        ' ... and this set's focus to txtFirstName.
+        txtFirstName.Focus()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnExitApp.Click
+        Me.Close()
+    End Sub
+End Class
